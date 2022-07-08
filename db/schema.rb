7a5a5326084735f8f70fe37364f3065b2ab3fe08
tyler-tran-goldenowl <tyler.tran.goldenowl@gmail.com>
2022-07-08 16:08:07 +0700

    Fix association and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_031123) do
+ActiveRecord::Schema.define(version: 2022_07_08_090204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,9 @@ ActiveRecord::Schema.define(version: 2022_07_08_031123) do
     t.integer "quantity", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "shop_id", null: false
     t.index ["product_id"], name: "index_cart_items_on_product_id"
+    t.index ["shop_id"], name: "index_cart_items_on_shop_id"
     t.index ["user_id"], name: "index_cart_items_on_user_id"
   end
 
@@ -138,6 +140,7 @@ ActiveRecord::Schema.define(version: 2022_07_08_031123) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "products"
+  add_foreign_key "cart_items", "shops"
   add_foreign_key "cart_items", "users"
   add_foreign_key "category_products", "categories"
   add_foreign_key "category_products", "products"
