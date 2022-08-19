43af074beebfc6f5a68fce720847e07622f40038
Thai <tathai2610@gmail.com>
2022-08-19 14:48:15 +0700

    Config deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,3 +39,5 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :passenger_restart_with_touch, true
