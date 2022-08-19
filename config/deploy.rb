b94f69c646a98ae69e18c824fb4b57ac9e63e3cf
Thai <tathai2610@gmail.com>
2022-08-19 12:05:25 +0700

    reinstall actiontext

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,8 @@ set :application, "goldenmall"
 set :repo_url, "git@github.com:tathai2610/GoldenOwlDemo.git"
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/#{fetch :application}"
